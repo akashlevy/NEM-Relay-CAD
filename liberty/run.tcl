392bc61b34638d16db73a0998bac7b9156bbdc15
Akash Levy <akashlevy@gmail.com>
2020-10-14 00:00:34 -0700

    Run

--- liberty/run.tcl
+++ liberty/run.tcl
@@ -7,8 +7,8 @@ create $charpoint
 set_log_file $charpoint/sis.log
 set_location $charpoint
 
-# Copy stuff over
-exec ln -s $charpoint/netlists $charpoint/../../spice/models
+# Symlink
+exec ln -s $charpoint/../../spice/models $charpoint/netlists 
 
 # Configure and characterize
 configure -fast -timing -power -ccs $cells
@@ -18,5 +18,8 @@ characterize $cells
 model -verilog -output verilog $cells
 model -timing -power -output nldm $cells
 model -timing -power -ccs -output ccs $cells
+
+# Compile models
+exec lc_shell -f compile.tcl
 
 log_info "IAMDONE"
