dc9f6fbf1edd7433c9f7f8701073ec02c2cd8a42
Akash Levy <akashlevy@gmail.com>
2020-10-31 04:50:31 -0700

    Update compile.tcl

--- liberty/compile.tcl
+++ liberty/compile.tcl
@@ -1,13 +1,13 @@
 #####################
 # Compile CCS model #
 #####################
+suppress_message { LBDB-29 LBDB-366 LBDB-75 LIBG-242 UIL-2 }
 read_lib ./nems40tt/models/liberty/ccs_nems40tt.lib
 write_lib nems40tt -f db -o ./nems40tt/models/liberty/ccs_nems40tt.db
 
 ######################
 # Compile NLDM model #
 ######################
-suppress_message { LBDB-29 LBDB-366 LBDB-75 LIBG-242 UIL-2 }
 read_lib ./nems40tt/models/liberty/nldm_nems40tt.lib
 write_lib nems40tt -f db -o ./nems40tt/models/liberty/nldm_nems40tt.db
 
