ef01b37a925e65191b1fe8c9a60b3936e9d2d697
Akash Levy <akashlevy@gmail.com>
2021-05-18 00:56:38 -0700

    Needs a cell name maybe?

--- liberty/run.tcl
+++ liberty/run.tcl
@@ -22,7 +22,7 @@ set cells [lmap f [glob -directory control -tails *.inst] {file rootname $f}]
 set newcells $cells
 
 # Use driver waveform
-import_driver -driver_db $charpoint/config/driver.db -pintypes_map {tcbn40ulpbwp40_c170815tt1p1v25c default}
+import_driver ndw -driver_db $charpoint/config/driver.db -pintypes_map {tcbn40ulpbwp40_c170815tt1p1v25c default}
 
 # Configure and characterize
 configure -fast -timing -power -ccs $newcells
