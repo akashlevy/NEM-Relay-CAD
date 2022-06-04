6f19f4a5092addbb21da129ac30e49b7ad557a99
Akash Levy <akashlevy@gmail.com>
2022-06-04 05:57:34 -0700

    Remove old script first

--- bench/ptpx.sh
+++ bench/ptpx.sh
@@ -2,6 +2,7 @@
 # Takes in top level design name as argument and
 # runs basic PTPX script
 setenv DESIGN $1
+rm -f scripts/constraints.sdc
 echo set_input_transition 0 \[all_inputs\] > scripts/constraints.sdc
 echo set_load $2 \[all_outputs\] >> scripts/constraints.sdc
 echo create_clock -name clk -period 5 >> scripts/constraints.sdc
