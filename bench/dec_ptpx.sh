89b2ec2316c63f067940cc011bb39ebf1e126bf4
Akash Levy <akashlevy@gmail.com>
2022-06-05 22:12:05 -0700

    dec_ptpx.sh

---
+++ bench/dec_ptpx.sh
@@ -0,0 +1,7 @@
+#! /bin/tcsh
+foreach nin (2 4 10)
+  foreach drive (0 1 2 3 4 6 8 12 16 20 24)
+    echo RUNNING: source ptpx.sh mux${nin} BUFFD${drive}BWP40 0.0025
+    source ptpx.sh mux${nin} BUFFD${drive}BWP40 0.0025
+  end
+end
