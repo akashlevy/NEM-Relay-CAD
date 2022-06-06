479791eac8d9efecfe74b244267e59fc416cab23
Akash Levy <akashlevy@gmail.com>
2022-06-06 09:06:05 -0700

    Try feedthru with different resistances

--- bench/process_delay.py
+++ bench/process_delay.py
@@ -7,7 +7,7 @@ pat2 = re.compile("data arrival time\s*([\S]+)")
 pat3 = re.compile("Startpoint: S.*Endpoint: Z", re.DOTALL)
 
 Ns, Ds, loads, delays, names = [], [], [], [], []
-for fname in glob.glob("ptpx/reports/mux*.timing.rpt"):
+for fname in glob.glob("ptpx/reports/muxs*.timing.rpt"):
   with open(fname) as f:
     print(fname)
 
@@ -50,23 +50,26 @@ for fname in glob.glob("ptpx/reports/feedthru*.timing.rpt"):
 data = pd.DataFrame({'N': Ns, 'D': Ds, 'load': loads, 'delay': delays, 'name': names})
 print(data.head())
 
-data = data[data['D'] <= 4]
+data = data[data['D'] == 1]
 data = data[data['load'] <= .100]
 
-cmosN2 = data[(data['N'] == 2) & (data['name'] == 'cmos')]
-cmosN4 = data[(data['N'] == 4) & (data['name'] == 'cmos')]
-cmosN10 = data[(data['N'] == 10) & (data['name'] == 'cmos')]
-nemsN2 = data[(data['N'] == 2) & (data['name'] == 'nems')]
-print (nemsN2.head())
-nemsN4 = data[(data['N'] == 4) & (data['name'] == 'nems')]
-print (nemsN4.head())
-nemsN10 = data[(data['N'] == 10) & (data['name'] == 'nems')]
+cmosN2 = data[(data['N'] == 2) & (data['name'] == 'cmos')].sort_values('load')
+cmosN4 = data[(data['N'] == 4) & (data['name'] == 'cmos')].sort_values('load')
+cmosN10 = data[(data['N'] == 10) & (data['name'] == 'cmos')].sort_values('load')
+nemsN2 = data[(data['N'] == 2) & (data['name'] == 'nems')].sort_values('load')
+nemsN4 = data[(data['N'] == 4) & (data['name'] == 'nems')].sort_values('load')
+nemsN10 = data[(data['N'] == 10) & (data['name'] == 'nems')].sort_values('load')
 
-ax = plt.axes(projection='3d')
-ax.plot_trisurf(cmosN2['D'], cmosN2['load']*1000, cmosN2['delay'], edgecolor='none')
-ax.plot_trisurf(cmosN4['D'], cmosN4['load']*1000, cmosN4['delay'], edgecolor='none')
-ax.plot_trisurf(cmosN10['D'], cmosN10['load']*1000, cmosN10['delay'], edgecolor='none')
-ax.plot_trisurf(nemsN2['D'], nemsN2['load']*1000, nemsN2['delay'], edgecolor='none')
-ax.plot_trisurf(nemsN4['D'], nemsN4['load']*1000, nemsN4['delay'], edgecolor='none')
-ax.plot_trisurf(nemsN10['D'], nemsN10['load']*1000, nemsN10['delay'], edgecolor='none')
+# plt.xscale('log')
+plt.figure(figsize=(6, 4))
+plt.xlabel("Load Capacitance (fF)")
+plt.ylabel("Delay (ns)")
+plt.plot(cmosN2['load']*1000, cmosN2['delay'], label="2i 8b CMOS")
+plt.plot(cmosN4['load']*1000, cmosN4['delay'], label="4i 8b CMOS")
+plt.plot(cmosN10['load']*1000, cmosN10['delay'], label="10i 8b CMOS")
+plt.plot(nemsN2['load']*1000, nemsN2['delay'], label="2i 8b NEMS ($R_{DS} = 80Ω$)")
+plt.plot(nemsN4['load']*1000, nemsN4['delay'], label="4i 8b NEMS ($R_{DS} = 80Ω$)")
+plt.plot(nemsN10['load']*1000, nemsN10['delay'], label="10i 8b NEMS ($R_{DS} = 80Ω$)")
+plt.legend(title="Mux I→Z Delay", loc='center left', bbox_to_anchor=(1, 0.5))
+plt.tight_layout()
 plt.show()
---
+++ bench/rtl/feedthru5k.v
@@ -0,0 +1,8 @@
+module feedthru5k (
+  input S,
+  output [0:0] Z
+);
+
+assign Z[0] = S;
+
+endmodule
